3863a8dcca95e810f4dc192e96917d1b2b0e9654
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-11 02:00:58 +0900

    New: asset > config > op.php, php.php

---
+++ asset/config/op.php
@@ -0,0 +1,22 @@
+<?php
+/**	op-skeleton-2030:/asset/config/op.php
+ *
+ * @created   2022-11-15  op-skeleton-2020:/asset/config/op.php
+ * @version   1.0
+ * @package   op-skeleton-2030
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/**	Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+//	...
+define('_OP_APP_BRANCH_', '2030');
